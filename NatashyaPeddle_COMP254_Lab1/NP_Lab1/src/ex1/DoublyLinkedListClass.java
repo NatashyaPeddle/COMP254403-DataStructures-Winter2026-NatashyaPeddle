38256046f3a2f375530af2490cea98c4919e47e0
NatashyaPeddle <natashya.peddle@gmail.com>
2026-01-28 12:40:53 -0500

    PUSH BEFORE ASSIGNMENT 1 START

---
+++ NatashyaPeddle_COMP254_Lab1/NP_Lab1/src/ex1/DoublyLinkedListClass.java
@@ -0,0 +1,12 @@
+package ex1;
+
+//In this exercise, you will use the DoublyLinkedList implementation of the textbook (week 2 lecture examples.
+//Write a method for concatenating two doubly linked lists L and M, with header and trailer sentinel nodes,
+//into a single list L′. Write a main method to test the new method. Hint: Connect the end of L into the beginning of M.
+
+public class DoublyLinkedListClass <E> {
+
+    /// LAYOUT: Previous <- element list -> next  double list = both ways
+
+
+}
